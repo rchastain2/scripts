580778c620f35f7930cd373e371952e6ec7bfa6e
Roland Chastain <roland.chastain@gmx.fr>
2026-04-05 10:26:48 +0200

    Add PasBuild installer

--- .bash_history
+++ .bash_history
@@ -1,1 +1,5 @@
 rm -fv .bash_history
+git add .
+git commit -m "Delete file"
+git push
+exit
---
+++ bash/install-pasbuild/install-pasbuild.sh
@@ -0,0 +1,19 @@
+
+## Installation de [PasBuild](https://github.com/graemeg/PasBuild) sous Linux
+
+DIR=pasbuild
+
+git clone --single-branch --depth 1 https://github.com/graemeg/PasBuild.git $DIR
+
+cd $DIR
+mkdir -p target/units
+sed 's/\${project.version}/1.0.0/g' src/main/resources/version.inc > target/version.inc
+
+fpc -Mobjfpc -O1 -FEtarget -FUtarget/units -Fitarget -Fusrc/main/pascal src/main/pascal/PasBuild.pas
+./target/PasBuild --version
+
+./target/PasBuild compile
+./target/pasbuild --version
+
+sudo cp -fv target/pasbuild /usr/local/bin/pasbuild
+pasbuild --version
